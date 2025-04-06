84f821215cada4f79cd0ce5e27c60c4a3b2e1020
Rustam Shakirzianov <rustam4m@Mac-mini-Rustam.local>
2025-04-06 22:59:06 +0300

    Added Onboarding screen

---
+++ FoodDeliveryApp_/FoodDeliveryApp_/UILayer/Onboarding/OnboardingViewPresenter.swift
@@ -0,0 +1,29 @@
+//
+//  OnboardingViewPresenter.swift
+//  FoodDeliveryApp_
+//
+//  Created by Rustam Shakirzianov on 06.04.2025.
+//
+
+import Foundation
+
+protocol OnboardingViewOutput: AnyObject {
+    func onboardingFinish()
+}
+
+class OnboardingViewPresenter: OnboardingViewOutput {
+    
+    //MARK: - Properties
+    weak var coordinator: OnboardingCoordinator!
+    
+    init(coordinator: OnboardingCoordinator!) {
+        self.coordinator = coordinator
+    }
+    
+    func onboardingFinish() {
+        coordinator.finish()
+    }
+    
+    
+}
+
